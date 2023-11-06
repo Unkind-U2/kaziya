1159c859ae8a1d29c3ae0725b8fea3ce15886d5a
yuto <gausslaw926@gmail.com>
2023-11-06 23:29:39 +0900

    add favicon in head tag

---
+++ wordpress/wp-content/themes/kaziya-stollen/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Thu Oct 19 2023 09:43:06 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="651bb35f96b829a772fa3f55" data-wf-site="651bb35f96b829a772fa3f52">
+<head>
+  <meta charset="utf-8">
+  <title>一関シュトーレン</title>
+  <meta name="description" content="極上の一関シュトーレン、クリスマスのお祝いにぴったり。株式会社かぢやとRestaurant & Catering Onのコラボ商品。贅沢な食材を農家さんから厳選。">
+  <meta name=”keywords” content=”一関シュトーレン,クリスマス,コラボ商品”/>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link rel="icon" href="<?php bloginfo('template_url');?>/images/logo.png">
+  <link href="<?php bloginfo('template_url');?>/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="<?php bloginfo('template_url');?>/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="<?php bloginfo('template_url');?>/css/kaziya.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Oswald:200,300,400,500,600,700","BIZ UDPMincho:regular","Noto Serif JP:200,300,regular,500,600,700,900:japanese,latin"]  }});</script>
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="<?php bloginfo('template_url');?>/images/logo.png" rel="shortcut icon" type="image/x-icon">
+  <link href="<?php bloginfo('template_url');?>/images/logo.png" rel="apple-touch-icon">
+  <?php wp_head(); ?>
+</head>
+<body class="body">
